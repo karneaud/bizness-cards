a0d9897198e11fd4cbfbd36855f62c22fce7ebd3
karneaud <kendall.arneaud@gmail.com>
2024-01-31 20:56:11 -0400

    return getter function

--- src/store/index.js
+++ src/store/index.js
@@ -15,14 +15,18 @@ export const state = () => ({
     themes() {return this.data.user.themes},
     cards() {
         return this.businesses
-    }
+    },
+    getBusiness(state) {  return (value) => state.data.businesses[value]/*filter(b => b.id !== value)*/ } 
   },
   actions: {
-    addBusiness(value) { if(!this.data) { this.data = { businesses: [], user: { themes: [] } } } this.data.businesses.push(value) },
+    setBusiness(value, id) { 
+      if(!this.data) this.data = { businesses: [], user: { themes: [] } } 
+      else if(id !== null) this.data.businesses[id] = value
+      else this.data.businesses.push(value) 
+    },
     removeBusiness (value) { return this.data.businesses = this.data.businesses.filter(b => b.id !== value.id)},
-    getBusiness(value) {  return this.data.businesses.filter(b => b.id !== value)} ,
     setUser(value) {  return this.data.user = value },
     addTheme (value) { return this.data.user.themes.push(value) },
-    removeTheme (value) { return this.data.user.themes = this.data.user.themes.filter(t => t.id !== value) },
+    removeTheme (value) { return this.data.user.themes = this.data.user.themes.filter(t => t.id !== value) }
   },
 })
