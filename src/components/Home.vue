b7f58383ab60f139fe60f779aef4c17813784232
karneaud <kendall.arneaud@gmail.com>
2024-01-31 20:56:53 -0400

    fix width and text wrapping

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,16 +16,21 @@
             <section v-else  class="_width:100%! _clearfix!">
             <h2>Business Cards</h2>
               <i-list-group :border="false" class="_width:100%!">
-                <i-list-group-item class="_border-bottom! _display:flex!"  v-for="(business, index) in businesses" :key="index" :to="`/card/${index}`">
-                  <span class="_text:left! _flex-grow:1!">{{ business.organization }}</span>
-                  <span class="_text:right!">&#x203A;</span>
+                <i-list-group-item class="_border-bottom! _display:flex! _padding-left:0! _width:100%"  v-for="(business, index) in businesses" :key="index">
+                  <div class="_padding-right:1 _flex:fill!"><router-link :to="`/business/${index}`"><v-icon name="la-edit"/></router-link></div>
+                  <router-link :to="`/card/${index}`" class="_text:left! _display:flex _max-width:90%">
+                    <span class="_text-overflow:ellipsis _padding-right:1 _width:inherit!"><strong>{{ business.organization }}</strong></span>
+                    <span class="_text:right!">&#x203A;</span>
+                  </router-link>
                 </i-list-group-item>
               </i-list-group>
               </section>
             </i-layout-content>
           <i-layout-footer v-if="hasBusinesses">
            <div class="_width:100% _margin:1!">
-              <i-button circle outline size="lg" :to="`/business`"><template #icon><i-icon name="ink-plus"/></template></i-button>
+              <i-button circle outline size="lg" :to="`/business`">
+                <template #icon><i-icon name="ink-plus"/></template>
+              </i-button>
            </div>
           </i-layout-footer>
         </i-layout>
