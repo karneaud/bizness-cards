cc4c29b2e261e4d860009bc0803afd856c757df3
karneaud <kendall.arneaud@gmail.com>
2024-01-31 20:55:50 -0400

    add icons

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import { createPinia } from 'pinia';
 import router from './routes';
 import App from './App.vue';
 import { Inkline, components } from '@inkline/inkline';
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { LaEdit } from "oh-vue-icons/icons";
+
 import './css/variables/index.scss';
 import '@inkline/inkline/css/index.scss';
 import '@inkline/inkline/css/utilities.scss';
 import './style.css';
 
-createApp(App).use(createPinia()).use(router).use(Inkline, { components }).mount('#app');
+
+addIcons(LaEdit);
+
+createApp(App).component('v-icon',OhVueIcon).use(createPinia()).use(router).use(Inkline, { components }).mount('#app');
