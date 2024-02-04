71b6960f1d7365cc36471b1f8ab751faa2c4c670
karneaud <kendall.arneaud@gmail.com>
2024-02-04 16:55:47 -0400

    remove icon component

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import { createPinia } from 'pinia';
 import router from './routes';
 import App from './App.vue';
 import { Inkline, components } from '@inkline/inkline';
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { LaEdit } from "oh-vue-icons/icons";
 
 import './css/variables/index.scss';
 import '@inkline/inkline/css/index.scss';
@@ -12,6 +10,4 @@ import '@inkline/inkline/css/utilities.scss';
 import './style.css';
 
 
-addIcons(LaEdit);
-
-createApp(App).component('v-icon',OhVueIcon).use(createPinia()).use(router).use(Inkline, { components }).mount('#app');
+createApp(App).use(createPinia()).use(router).use(Inkline, { components }).mount('#app');
