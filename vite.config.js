1e4d3d925867342a14d4b9bbe2252f2b231180be
karneaud <kendall.arneaud@gmail.com>
2024-02-04 16:55:24 -0400

    add pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import inkline from '@inkline/plugin/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const inklineConfig = {
     outputDir: 'src/css/variables'
@@ -8,7 +9,35 @@ const inklineConfig = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), inkline(inklineConfig) ],
+    plugins: [vue(), inkline(inklineConfig), VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        includeAssets: ['favicon.svg'],
+        manifest: {
+          name: 'BiznzCard',
+          short_name: 'BiznzCard',
+          description: 'Digital business cards',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'favicon.svg',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'favicon.svg',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })  ],
+      
+    define: {
+        _global: ({})
+    },
     optimizeDeps : { extension: ['.jsx', '.vue'] },
     server: {
         host: '0.0.0.0',
