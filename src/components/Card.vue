7fbc26fb45233ef8aa5ec492a61cd1ddeb5d08eb
karneaud <kendall.arneaud@gmail.com>
2024-02-04 16:57:04 -0400

    revise markup

---
+++ src/components/Card.vue
@@ -0,0 +1,82 @@
+<template>
+  <section :class="`theme-card ${ card.theme || 'default' }`">
+   <article class="name"><span class="first-name">{{ card.firstName }}</span><span class="last-name">{{ card.lastName }}</span></article>
+    <article class="company"><span class="text">{{ card.organization }}</span></article>
+    <article class="job"><span class="text">{{ card.job }}</span></article>
+    <article class="phone">
+      <span class="heading"><i class=" bx bxs-phone bx-lg"></i><strong>Phone:</strong></span>
+      <a target="_blank" class="link" :href="`tel:${card.phone}`">
+        <i class=" bx bxs-phone bx-sm"></i>
+        <span class="text">{{ card.phone }}</span>
+      </a>
+      <a target="_blank" class="qr-code" @click="show('phone')"> 
+        <i class='bx bx-qr bx-sm' ></i>
+        <vue-qrcode type="image/png"  class="hidden" :value="card.phone" :color="color"></vue-qrcode>
+      </a>
+    </article>
+   <article class="email">
+      <span class="heading"><i class="bx bx-envelope bx-lg"></i><strong>Email:</strong></span>
+      <a target="_blank" class="link" :href="`mailto:${card.email}`">
+        <i class="bx bxs-envelope bx-sm"></i>
+        <span class="text">{{ card.email }}</span>
+      </a>
+      <a class="qr-code" @click="show('email')"> 
+        <i class='bx bx-qr bx-sm' ></i>
+        <vue-qrcode type="image/png"  class="hidden" :value="card.email" :color="color"></vue-qrcode>
+      </a>
+    </article>
+    <article class="website">
+        <span class="heading"><i class="bx bx-globe bx-lg"></i><strong>Website:</strong></span>
+        <a target="_blank" class="link" :href="card.website"><i class=" bx bx-globe bx-sm"></i>
+          <span class="text">{{ card.website }}</span>
+         
+        </a>
+         <a class="qr-code" @click="show('website')"> 
+          <i class='bx bx-qr bx-sm' ></i>
+            <vue-qrcode type="image/png"  class="hidden" :value="card.website" :color="color"></vue-qrcode>
+          </a>
+      </article>
+    <article class="social">
+      <div v-for="(link, index) in card.socialMedia" :key="index" :class="`${mediaName(link)} platform`">
+          <span :class="`heading  social-${ mediaName(link) }`"><i :class="`bx bxl-${ mediaName(link) } bx-sm`"></i><strong>{{ mediaName(link) }}:</strong></span>
+          <a target="_blank" class="link" :href="link">
+            <i :class="` bx bxl-${ mediaName(link) } bx-sm`"></i>
+            <span :class="`text social-${ mediaName(link) }`">@{{ socialHandle(link) }}</span>
+          </a>
+          <a class="qr-code" @click="show(`${mediaName(link)}`)"> 
+            <i class='bx bx-qr bx-sm' ></i>
+            <vue-qrcode type="image/png"  class="hidden" :value="link" :color="color"></vue-qrcode>
+          </a>
+      </div>
+    </article>
+    <article class="vcard">
+      <span class="heading"><i class="bx bx-id-card bx-lg"></i><strong>vCard</strong></span>
+      <a class="qr-code" @click="show('vcard')">
+        <i class="bx bx-id-card bx-sm"></i>
+        <vue-qrcode type="image/png" :value="vCard" :color="color"></vue-qrcode>
+      </a>
+    </article>
+  </section>
+</template>
+<style lang="sass" scoped>
+  @import "../themes/default"
+</style>
+<script setup>
+    import router from '../routes'
+    import { useMainStore } from '../store'
+    import { storeToRefs } from 'pinia'
+    import createVCard from '../lib/vcard.js'
+    import VueQrcode from 'vue-qrcode'
+
+    const store = useMainStore(), { getBusiness: { value: getBusinessById } } = storeToRefs(store), id = router.currentRoute.value.params.id, card = getBusinessById( id ), color = { dark: '#000000ff', light: '#ffffffff' } , vCard = createVCard(card), 
+    mediaName = (url) => {
+      let m = url.match(/^(?:https?:\/\/)?(?:www\.)?(?!www\.)(([^\/]+)\.[a-z]+)/)
+      return m[2]
+    }, socialHandle = (url) => { 
+      let m = url.match(/\/([^\/]+)\/?$/)
+      return m[1]
+    }, show = (code) => {
+        document.querySelector(`.${code}`).classList.toggle('show')
+        return false;
+    }
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,36 +2,56 @@
   import { useMainStore } from '../store'
   import router from '../routes'
 
-  const store = useMainStore(),  cards = store.cards, createProfile = () => router.push('/business', null ), businesses = store.businesses, hasBusinesses = businesses.length > 0
+  const store = useMainStore(),  cards = store.cards, createProfile = () => router.push('/business', null ), businesses = store.businesses, hasBusinesses = businesses.length > 0, remove = (index) => store.removeBusiness(index)
 
 </script>
     <template>
         <i-layout class="_width:100%! _height:100%">
           <i-layout-content>
-            <section v-if="!hasBusinesses">
+            <article v-if="!hasBusinesses">
               <h1>Create Card</h1>
               <p>To begin...Let's create a business profile</p>
               <i-button id="create-button" circle outline size="lg" color="primary" @click="createProfile">Create</i-button>
-            </section>
-            <section v-else  class="_width:100%! _clearfix!">
+            </article>
+            <article v-else  class="_width:100%! _clearfix!">
             <h2>Business Cards</h2>
               <i-list-group :border="false" class="_width:100%!">
                 <i-list-group-item class="_border-bottom! _display:flex! _padding-left:0! _width:100%"  v-for="(business, index) in businesses" :key="index">
-                  <div class="_padding-right:1 _flex:fill!"><router-link :to="`/business/${index}`"><v-icon name="la-edit"/></router-link></div>
-                  <router-link :to="`/card/${index}`" class="_text:left! _display:flex _max-width:90%">
-                    <span class="_text-overflow:ellipsis _padding-right:1 _width:inherit!"><strong>{{ business.organization }}</strong></span>
+                  <div class="_padding-right:1 _flex:fill!"><router-link :to="`/business/${index}`"><i class="bx bx-edit bx-sm"></i></router-link></div>
+                  <router-link :to="`/card/${index}`" class="_text:left! _display:flex _max-width:90% _width:100vw">
+                    <span class="_text-overflow:ellipsis _padding-right:1 _width:100%!"><strong>{{ business.organization }}</strong></span>
                     <span class="_text:right!">&#x203A;</span>
                   </router-link>
+                  <div class="_flex:fill! _padding-left:1"><a @click="remove(index)"><i class="bx bx-minus-circle bx-sm"></i></a></div>
                 </i-list-group-item>
               </i-list-group>
-              </section>
+              </article>
             </i-layout-content>
           <i-layout-footer v-if="hasBusinesses">
            <div class="_width:100% _margin:1!">
-              <i-button circle outline size="lg" :to="`/business`">
+              <i-button circle outline primary btn-primary size="lg" :to="`/business`">
                 <template #icon><i-icon name="ink-plus"/></template>
               </i-button>
            </div>
           </i-layout-footer>
         </i-layout>
     </template>
+    <style lang="css" scoped>
+      button#create-button {
+        display: flex;
+        margin: 0 auto;
+        padding: 20%;
+      }
+
+      .layout-content, .layout-footer {
+        padding: 8px;
+      }
+
+      #app > main > section {
+        height: 100%;
+        width: 100%;
+        padding: 24px;
+      }
+
+
+    </style> 
