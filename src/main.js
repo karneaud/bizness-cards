ba68b2f5637e3c7cff76a70b54dfb984caaffaca
karneaud <kendall.arneaud@gmail.com>
2024-02-08 12:11:31 -0400

    register components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './routes';
 import App from './App.vue';
+import CardStack  from './components/stack/CardStack.vue';
+import StackCard from './components/stack/StackCard.vue';
 import { Inkline, components } from '@inkline/inkline';
 
 import './css/variables/index.scss';
@@ -9,5 +11,4 @@ import '@inkline/inkline/css/index.scss';
 import '@inkline/inkline/css/utilities.scss';
 import './style.css';
 
-
-createApp(App).use(createPinia()).use(router).use(Inkline, { components }).mount('#app');
+createApp(App).use(createPinia()).use(router).use(Inkline, { components }).component('card-stack', CardStack).component('stack-card', StackCard).mount('#app');
