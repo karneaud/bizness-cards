fdf96d1e1d7a9b265358b3d9e1673c088ae7b5f4
karneaud <kendall.arneaud@gmail.com>
2024-02-15 12:16:32 -0400

    remove layout component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,32 +6,21 @@
 
 </script>
     <template>
-        <i-layout class="_width:100%! _height:100%">
-          <i-layout-content class="_overflow:hidden _display:flex _justify-content:center _align-items:center">
-            <article v-if="!hasBusinesses">
+        <article v-if="!hasBusinesses">
               <h1>Create Card</h1>
               <p>To begin...Let's create a business profile</p>
               <i-button id="create-button" circle outline size="lg" color="primary" @click="createProfile">Create</i-button>
             </article>
-            <article v-else  class="_width:100%! _height:100%! _overflow:hidden _flex-direction:column _display:flex _align-items:center _justify-content:center ">
+            <article v-else class="_width:100%! _height:100%! _overflow:hidden _flex-direction:column _display:flex _align-items:center _justify-content:center ">
             <h2>Business Cards</h2>
               <card-stack>
                 <template v-slot="slotProps">
-                  <stack-card @swipe="slotProps.handleSwipeEvent" v-for="(i,index) in [1,2,3,4,5]" ref="stackedCards" :key="index" :style="{ zIndex: i }" :isActive="`${ i == 5}`" :id="`card-${i}-${index}`">
+                  <stack-card @swiping="slotProps.handleSwipeEvent" v-for="(i,index) in [1,2,3,4,5]" ref="stackedCards" :key="index"  :zIndex="i" :id="`card-${i}-${index}`">
                     <div class=""><p>Card {{ i }}</p></div>
                   </stack-card>
                 </template>
               </card-stack>
               </article>
-            </i-layout-content>
-          <i-layout-footer v-if="hasBusinesses">
-           <div class="_width:100% _margin:1!">
-              <i-button circle outline primary btn-primary size="lg" :to="`/business`">
-                <template #icon><i-icon name="ink-plus"/></template>
-              </i-button>
-           </div>
-          </i-layout-footer>
-        </i-layout>
     </template>
     <style lang="css" scoped>
       button#create-button {
