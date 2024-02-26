6d36bfe8f490e06853fb9b9136b2e0cf233d750c
karneaud <kendall.arneaud@gmail.com>
2024-02-26 18:36:14 -0400

    simplify form structure

--- src/components/Business.vue
+++ src/components/Business.vue
@@ -1,49 +1,38 @@
 <template>
   <section>
-    <i-form @submit="submitForm">
-      <i-form-group>
+    <i-form @submit="submitForm" class="_display:flex! _flex-direction:column _justify-content:space-between _height:99vh _padding-y:1">
+     <!-- <i-form-group>
         <i-label for="logo">Logo</i-label>
         <i-input type="file" id="logo" v-model.lazy="business.logo" />
       </i-form-group>
-
+    -->
+    
       <i-form-group>
-        <i-label for="organization">Organization</i-label>
-        <i-input type="text" id="organization" v-model.lazy="business.organization" />
+        <i-input placeholder="Organization" type="text" id="organization" v-model.lazy="business.organization" />
       </i-form-group>
 
       <i-form-group>
-        <i-label for="phone">Phone</i-label>
-        <i-input type="number" id="phone" v-model.lazy="business.phone" />
+        <i-input placeholder="Phone" type="number" id="phone" v-model.lazy="business.phone" />
       </i-form-group>
 
       <i-form-group>
-        <i-label for="email">Email</i-label>
-        <i-input type="email" id="email" v-model.lazy="business.email" />
+        <i-input placeholder="Email" type="email" id="email" v-model.lazy="business.email" />
       </i-form-group>
 
-      <div class="_display:flex">
-        <i-form-group>
-          <i-label for="firstName">First Name</i-label>
-          <i-input type="text" id="firstName" v-model.lazy="business.firstName" />
+        <i-form-group class="_display:flex">
+          <i-input size="sml" placeholder="First Name" type="text" id="firstName" v-model.lazy="business.firstName" />   
+          <i-input size="sml" placeholder="Last Name" type="text" id="lastName" v-model.lazy="business.lastName" />
         </i-form-group>
           <i-form-group>
-          <i-label for="lastName">Last Name</i-label>
-          <i-input type="text" id="lastName" v-model.lazy="business.lastName" />
+          <i-input type="text" id="job" v-model.lazy="business.job" placeholder="Job Title" />
         </i-form-group>
-      </div>
-          <i-form-group>
-          <i-label for="job">Job Title</i-label>
-          <i-input type="text" id="job" v-model.lazy="business.job" />
-        </i-form-group>
-      <i-form-group>
-        <i-label for="website">Website</i-label>
-        <i-input type="url" id="website" v-model.lazy="business.website" />
+      <i-form-group class="_margin-bottom:1!">
+        <i-input type="url" placeholder="Website" id="website" v-model.lazy="business.website" />
       </i-form-group>
 
       <div v-for="(link, index) in business.socialMedia" :key="index">
         <i-form-group>
-          <i-label :for="`social-media-${index}`">Social Media</i-label>
-          <i-input type="text" :id="`social-media-${index}`" v-model.lazy="business.socialMedia[index]"> 
+          <i-input placeholder="Social Media URL" type="text" :id="`social-media-${index}`" v-model.lazy="business.socialMedia[index]"> 
             <template #prepend>
                 <i-button @click="removeSocialMediaLink(index)">-</i-button>
             </template>
@@ -63,12 +52,11 @@
 </template>
 
 <script setup>
-  import { reactive, toRefs } from 'vue'
-  import router from '../routes'
+  import { reactive } from 'vue'
   import { useMainStore } from '../store'
   import { storeToRefs } from 'pinia'
 
-  const store = useMainStore(), { getBusiness: { value: getBusinessById } } = storeToRefs(store), id = router.currentRoute.value.params.id || null, business = getBusinessById( id ) || reactive({
+  const store = useMainStore(), { getBusiness: { value: getBusinessById } } = storeToRefs(store), id = null, business = getBusinessById( id ) || reactive({
     logo: '',
     organization: '',
     phone: '',
@@ -87,6 +75,22 @@
     business.socialMedia.splice(index, 1)
   }, submitForm = () => {
     store.setBusiness(business, id )
-    router.push('/')
   }
 </script>
+<style lang="css">
+ .input input {
+   transition: all .2s ;
+ }
+
+ .input input:focus
+ {
+    height: 54px;
+    color: 125%;
+ }
+
+@media (prefers-color-scheme: dark) {
+    input, .input {
+        background-color: transparent !important;
+    }
+}
+</style>
